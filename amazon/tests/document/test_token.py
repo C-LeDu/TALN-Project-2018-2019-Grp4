e662a7e325b16f065765ed09ae76678a7a139335
Gauriat <charlesgauriat@hotmail.fr>
2018-10-05 12:18:02 +0200

    Ajout Test_Token

---
+++ amazon/tests/document/test_token.py
@@ -0,0 +1,24 @@
+import logging
+import unittest
+import os
+import random
+from amazon.document import Token
+LOGGER = logging.getLogger(__name__)
+
+
+class test_token(unittest.TestCase):
+    def setUp(self):
+        self.token = Token(None, 3, 10, 'pos', 7, "Bonjour" )
+    # Configurer les tests unitaires (si nécessaire)
+
+    def test_text(self):
+        self.assertEqual(self.token.text,'Bonjour', 'Le mot \'est pas correct')
+
+    def test_pos(self):
+        self.assertEqual(self.token.pos,'pos', 'La pos n\'est pas correct')
+
+    def test_shape(self):
+        self.assertEqual(self.token.shape, 7 , 'La shape n\'est pas correct')
+
+if __name__ == '__main__':
+    unittest.main()
