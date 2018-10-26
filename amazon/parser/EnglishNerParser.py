6883e8b94f852b8dae68bfeed420faa33bc39eda
Gauriat <charlesgauriat@hotmail.fr>
2018-10-26 12:58:56 +0200

    ajout de parser version 1.0

--- amazon/parser/EnglishNerParser.py
+++ amazon/parser/EnglishNerParser.py
@@ -1,3 +1,4 @@
+from amazon.document.interval import Interval
 from amazon.document.document import Document
 from amazon.parser.parser import Parser
 
@@ -5,10 +6,25 @@ class EnglishNerParser(Parser):
     def read(self, content: str) -> Document:
         """Reads the content of a NER/POS data file and returns one document instance per document it finds."""
         documents = []
-
         # 1. Split the text in documents using string '-DOCSTART- -X- O O' and loop over it
-        # 2. Slit lines and loop over
-        # 3. Make vectors of tokens and labels (colunn 4) and at the '\n\n' make a sentence
-        # 4. Create a Document object
+        content = content.split('-DOCSTART- -X- O O')
+        for doc in content:
+            words = []
+            sentences = []
+            labels = []
+            start = 0
+            # 2. Split lines and loop over
+            str_sentences = doc.split('\n\n')
+            # 3. Make vectors of tokens and labels (colunn 4) and at the '\n\n' make a sentence
+            for sentence in str_sentences:
+                tokens = sentence.split('\n')
+                for token in tokens:
+                    cols = token.split(' ')
+                    words.append(cols[0])
+                    labels.append(cols[3])
+                sentences.append(Interval(start, start+len(tokens)))
+                start += len(tokens)
+            # 4. Create a Document object
+            documents.append(Document.create_from_vectors(words, sentences, labels))
 
         return documents
--- amazon/tests/parser/parser_test.py
+++ amazon/tests/parser/parser_test.py
@@ -2,23 +2,20 @@ import logging
 import unittest
 import os
 import random
-from amazon.document import Token
+from amazon.parser.EnglishNerParser import EnglishNerParser
 LOGGER = logging.getLogger(__name__)
 
+filename = 'F:/Utilisateur/Documents/ESGI/Cours/Traitement Automatique du Langage Naturel/data/testoiyer.txt'
 
 class test_parser(unittest.TestCase):
     def setUp(self):
-        self.token = Token(None, 3, 10, 'pos', 7, "Bonjour" )
+        with open(filename, 'r', encoding='utf-8') as fp:
+            self.content = fp.read()
     # Configurer les tests unitaires (si nécessaire)
 
-    def test_text(self):
-        self.assertEqual(self.token.text,'Bonjour', 'Le mot \'est pas correct')
-
-    def test_pos(self):
-        self.assertEqual(self.token.pos,'pos', 'La pos n\'est pas correct')
-
-    def test_shape(self):
-        self.assertEqual(self.token.shape, 7 , 'La shape n\'est pas correct')
+    def test_read(self):
+        documents = EnglishNerParser().read(self.content)
+        self.assertEqual(len(documents), 216, 'Gnééé')
 
 if __name__ == '__main__':
     unittest.main()
