c60ef58a03a86196497d70d555a5f7ffd8c3f317
Gauriat <charlesgauriat@hotmail.fr>
2018-12-07 14:49:04 +0100

    ajout de parser version 1.0

---
+++ amazon/tests/recurrentNeuralNetwork/test_recurrentNeuralNetwork.py
@@ -0,0 +1,26 @@
+import logging
+import unittest
+
+from amazon.parser.EnglishNerParser import EnglishNerParser
+from amazon.vectorizer.Vectorizer import Vectorizer
+from amazon.recurrentneuralnetwork.RecurrentNeuralNetwork import RecurrentNeuralNetwork
+
+filename = "F:/Utilisateur/Documents/ESGI/Cours/Traitement Automatique du Langage Naturel/Project/TALN-Project-2018-2019-Grp4/amazon/tests/vectorizer/testoiyage.txt"
+
+class test_recurrentNeuralNetwork(unittest.TestCase):
+
+
+    def setUp(self):
+        with open(filename, 'r', encoding='utf-8') as fp:
+            content = fp.read()
+            self.documents = EnglishNerParser().read(content)
+            self.vec = Vectorizer(
+                "F:/Utilisateur/Documents/ESGI/Cours/Traitement Automatique du Langage Naturel/glove.6B.50d.txt")
+        # (_, self.input_shape) = self.vec.encode_features(self.documents)
+
+
+    def test_build_sequence(self):
+        self.assertIsInstance(RecurrentNeuralNetwork.build_sequence(self.vec.word_embeddings,{"pos": (10,10), "shape": (2,2)},45), RecurrentNeuralNetwork)
+
+if __name__ == '__main__':
+    unittest.main()
